488f3f34f5ebc7a1656ef63b03dd742537896264
adaspera <jusciokas1@gmail.com>
2025-01-24 15:09:37 +0200

    Ui lib setup

--- PathFinder.client/src/App.tsx
+++ PathFinder.client/src/App.tsx
@@ -1,26 +1,33 @@
-import { useEffect, useState } from 'react'
-import apiClient from './api/apiService';
-
+import React from 'react'
+import { MapContainer, TileLayer } from 'react-leaflet'
+import {Button, buttonVariants} from "@/components/ui/button.tsx";
+import {cn} from "@/lib/utils.ts";
 
 const App: React.FC = () => {
-    const [data, setData] = useState<string[]>([]);
+    console.log(cn(buttonVariants({})));
 
-    useEffect(() => {
-        apiClient.get('/test') // Replace with your backend endpoint
-            .then(response => setData(response.data))
-            .catch(error => console.error(error));
-    }, []);
+
+    // return (
+    //     <div style={{ height: '100vh', width: '80%' }}>
+    //         <Button className="bg-zinc-950">AAA</Button>
+    //         <MapContainer
+    //             center={[51.505, -0.09]}
+    //             zoom={13}
+    //             style={{ height: '100%', width: '100%' }}
+    //         >
+    //             <TileLayer
+    //                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    //                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    //             />
+    //         </MapContainer>
+    //     </div>
+    // );
 
     return (
-        <div>
-            <h1>Data from Backend</h1>
-            <ul>
-                {data.map((item, index) => (
-                    <li key={index}>{item}</li>
-                ))}
-            </ul>
+        <div className="bg-amber-600">
+            <Button>aaaaa</Button>
         </div>
     );
 };
 
-export default App
+export default App;
